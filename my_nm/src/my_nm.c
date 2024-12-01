e233e4258474d9d11bfa26d32b6458eb53e9ed63
twip4 <paulbaudinot@gmail.com>
2024-12-02 00:15:11 +0100

    fix(clang fromat)

--- my_nm/src/my_nm.c
+++ my_nm/src/my_nm.c
@@ -6,149 +6,169 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-int check_elf(const char *filename) {
-  int fd = open(filename, O_RDONLY);
-  if (fd == -1) {
-    fprintf(stderr, "my_nm: Error: Cannot open file in read-only\n");
-    return -1;
-  }
-
-  char magic[4];
-  size_t bytes_read = read(fd, magic, sizeof(magic));
-  if (bytes_read != sizeof(magic)) {
-    fprintf(stderr, "my_nm: Error: Cannot read file\n");
-    return -1;
-  }
-
-  if (magic[0] == 0x7f && magic[1] == 'E' && magic[2] == 'L' && magic[3] == 'F')
-    return fd;
-  else {
-    fprintf(stderr, "my_nm: Error: Not an ELF file\n");
-    return -1;
-  }
-}
-
-void print_symbol(Elf64_Sym *symbol, char *section_str, char *symbol_str) {
-  char bind = ELF64_ST_BIND(symbol->st_info);
-  char *bind_str = NULL;
-  switch (bind) {
-  case STB_LOCAL:
-    bind_str = "STB_LOCAL";
-    break;
-  case STB_GLOBAL:
-    bind_str = "STB_GLOBAL";
-    break;
-  case STB_WEAK:
-    bind_str = "STB_WEAK";
-    break;
-  default:
-    bind_str = "STB_UNKNOWN";
-    break;
-  }
-
-  char type = ELF64_ST_TYPE(symbol->st_info);
-  char *type_str = NULL;
-  switch (type) {
-  case STT_NOTYPE:
-    type_str = "STT_NOTYPE";
-    break;
-  case STT_OBJECT:
-    type_str = "STT_OBJECT";
-    break;
-  case STT_FUNC:
-    type_str = "STT_FUNC";
-    break;
-  case STT_SECTION:
-    type_str = "STT_SECTION";
-    break;
-  case STT_FILE:
-    type_str = "STT_FILE";
-    return;
-  default:
-    type_str = "STT_UNKNOWN";
-    break;
-  }
-
-  char vis = ELF64_ST_VISIBILITY(symbol->st_other);
-  char *vis_str = NULL;
-  switch (vis) {
-  case STV_DEFAULT:
-    vis_str = "STV_DEFAULT";
-    break;
-  case STV_INTERNAL:
-    vis_str = "STV_INTERNAL";
-    break;
-  case STV_HIDDEN:
-    vis_str = "STV_HIDDEN";
-    break;
-  case STV_PROTECTED:
-    vis_str = "STV_PROTECTED";
-    break;
-  default:
-    vis_str = "SRV_UNKNOWN";
-    break;
-  };
-
-  printf("%016lx\t%lu\t%s\t%s\t%s\t%s\t%s\n", symbol->st_value, symbol->st_size,
-         type_str, bind_str, vis_str, section_str, symbol_str);
-}
-
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "my_nm: Usage: ./my_nm <file_path>\n");
-    return -1;
-  }
-
-  int fd = check_elf(argv[1]);
-  if (fd == -1)
-    return 1;
-
-  struct stat s = {0};
-  if ((stat(argv[1], &s)) == -1) {
-    fprintf(stderr, "my_nm: Error: Cannot stat(filename)");
-    close(fd);
-    return 1;
-  }
-
-  Elf64_Ehdr *elf_header = mmap(NULL, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-  if (elf_header == MAP_FAILED) {
-    fprintf(stderr, "my_nm: Error: Cannot mmap for the file size");
-    close(fd);
-    return 1;
-  }
-
-  Elf64_Shdr *section_headers =
-      (Elf64_Shdr *)((char *)elf_header + elf_header->e_shoff);
-
-  Elf64_Shdr shstrtab_header = section_headers[elf_header->e_shstrndx];
-  char *shstrtab = (char *)elf_header + shstrtab_header.sh_offset;
-
-  for (int i = 0; i < elf_header->e_shnum; i++) {
-    if (section_headers[i].sh_type == SHT_SYMTAB) {
-      Elf64_Shdr strtab_header = section_headers[section_headers[i].sh_link];
-      char *strtab = (char *)elf_header + strtab_header.sh_offset;
-
-      Elf64_Sym *symbols =
-          (Elf64_Sym *)((char *)elf_header + section_headers[i].sh_offset);
-      int count = section_headers[i].sh_size / sizeof(Elf64_Sym);
-
-      for (int j = 0; j < count; j++) {
-        char *symbol_name = &strtab[symbols[j].st_name];
-
-        if (symbols[j].st_shndx > elf_header->e_ehsize)
-          continue;
-
-        print_symbol(
-            &symbols[j],
-            symbols[j].st_shndx != 0
-                ? &shstrtab[section_headers[symbols[j].st_shndx].sh_name]
-                : "UND",
-            *symbol_name != '\0' ? symbol_name : "");
-      }
+int check_elf(const char *filename)
+{
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        fprintf(stderr, "my_nm: Error: Cannot open file in read-only\n");
+        return -1;
     }
-  }
 
-  close(fd);
-  munmap(elf_header, s.st_size);
-  return 0;
+    char magic[4];
+    size_t bytes_read = read(fd, magic, sizeof(magic));
+    if (bytes_read != sizeof(magic))
+    {
+        fprintf(stderr, "my_nm: Error: Cannot read file\n");
+        return -1;
+    }
+
+    if (magic[0] == 0x7f && magic[1] == 'E' && magic[2] == 'L'
+        && magic[3] == 'F')
+        return fd;
+    else
+    {
+        fprintf(stderr, "my_nm: Error: Not an ELF file\n");
+        return -1;
+    }
+}
+
+void print_symbol(Elf64_Sym *symbol, char *section_str, char *symbol_str)
+{
+    char bind = ELF64_ST_BIND(symbol->st_info);
+    char *bind_str = NULL;
+    switch (bind)
+    {
+    case STB_LOCAL:
+        bind_str = "STB_LOCAL";
+        break;
+    case STB_GLOBAL:
+        bind_str = "STB_GLOBAL";
+        break;
+    case STB_WEAK:
+        bind_str = "STB_WEAK";
+        break;
+    default:
+        bind_str = "STB_UNKNOWN";
+        break;
+    }
+
+    char type = ELF64_ST_TYPE(symbol->st_info);
+    char *type_str = NULL;
+    switch (type)
+    {
+    case STT_NOTYPE:
+        type_str = "STT_NOTYPE";
+        break;
+    case STT_OBJECT:
+        type_str = "STT_OBJECT";
+        break;
+    case STT_FUNC:
+        type_str = "STT_FUNC";
+        break;
+    case STT_SECTION:
+        type_str = "STT_SECTION";
+        break;
+    case STT_FILE:
+        type_str = "STT_FILE";
+        return;
+    default:
+        type_str = "STT_UNKNOWN";
+        break;
+    }
+
+    char vis = ELF64_ST_VISIBILITY(symbol->st_other);
+    char *vis_str = NULL;
+    switch (vis)
+    {
+    case STV_DEFAULT:
+        vis_str = "STV_DEFAULT";
+        break;
+    case STV_INTERNAL:
+        vis_str = "STV_INTERNAL";
+        break;
+    case STV_HIDDEN:
+        vis_str = "STV_HIDDEN";
+        break;
+    case STV_PROTECTED:
+        vis_str = "STV_PROTECTED";
+        break;
+    default:
+        vis_str = "SRV_UNKNOWN";
+        break;
+    };
+
+    printf("%016lx\t%lu\t%s\t%s\t%s\t%s\t%s\n", symbol->st_value,
+           symbol->st_size, type_str, bind_str, vis_str, section_str,
+           symbol_str);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "my_nm: Usage: ./my_nm <file_path>\n");
+        return -1;
+    }
+
+    int fd = check_elf(argv[1]);
+    if (fd == -1)
+        return 1;
+
+    struct stat s = { 0 };
+    if ((stat(argv[1], &s)) == -1)
+    {
+        fprintf(stderr, "my_nm: Error: Cannot stat(filename)");
+        close(fd);
+        return 1;
+    }
+
+    Elf64_Ehdr *elf_header =
+        mmap(NULL, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (elf_header == MAP_FAILED)
+    {
+        fprintf(stderr, "my_nm: Error: Cannot mmap for the file size");
+        close(fd);
+        return 1;
+    }
+
+    Elf64_Shdr *section_headers =
+        (Elf64_Shdr *)((char *)elf_header + elf_header->e_shoff);
+
+    Elf64_Shdr shstrtab_header = section_headers[elf_header->e_shstrndx];
+    char *shstrtab = (char *)elf_header + shstrtab_header.sh_offset;
+
+    for (int i = 0; i < elf_header->e_shnum; i++)
+    {
+        if (section_headers[i].sh_type == SHT_SYMTAB)
+        {
+            Elf64_Shdr strtab_header =
+                section_headers[section_headers[i].sh_link];
+            char *strtab = (char *)elf_header + strtab_header.sh_offset;
+
+            Elf64_Sym *symbols = (Elf64_Sym *)((char *)elf_header
+                                               + section_headers[i].sh_offset);
+            int count = section_headers[i].sh_size / sizeof(Elf64_Sym);
+
+            for (int j = 0; j < count; j++)
+            {
+                char *symbol_name = &strtab[symbols[j].st_name];
+
+                if (symbols[j].st_shndx > elf_header->e_ehsize)
+                    continue;
+
+                print_symbol(
+                    &symbols[j],
+                    symbols[j].st_shndx != 0
+                        ? &shstrtab[section_headers[symbols[j].st_shndx]
+                                        .sh_name]
+                        : "UND",
+                    *symbol_name != '\0' ? symbol_name : "");
+            }
+        }
+    }
+
+    close(fd);
+    munmap(elf_header, s.st_size);
+    return 0;
 }
--- my_strace/src/my_strace.c
+++ my_strace/src/my_strace.c
@@ -1,3 +1,5 @@
+#include "my_strace.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/ptrace.h>
@@ -7,110 +9,122 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#include "my_strace.h"
-
-char *get_syscall_name(long syscall_number) {
-  int syscall_count = sizeof(syscall_names) / sizeof(char *);
-  return (syscall_number < 0 || syscall_number >= syscall_count)
-             ? "Unknown syscall"
-             : syscall_names[syscall_number];
+char *get_syscall_name(long syscall_number)
+{
+    int syscall_count = sizeof(syscall_names) / sizeof(char *);
+    return (syscall_number < 0 || syscall_number >= syscall_count)
+        ? "Unknown syscall"
+        : syscall_names[syscall_number];
 }
 
-SysCall *getArgSyscall(char *name) {
-  for (int i = 0; i < 80; i++) {
-    if (!strcmp(sysArgList[i].name, name)) {
-      return &sysArgList[i];
+SysCall *getArgSyscall(char *name)
+{
+    for (int i = 0; i < 80; i++)
+    {
+        if (!strcmp(sysArgList[i].name, name))
+        {
+            return &sysArgList[i];
+        }
     }
-  }
-  return NULL;
+    return NULL;
 }
 
 void printSyscallArgs(SysCall *sysArg, struct user_regs_struct regs,
-                      int syscall_number) {
-  if (sysArg) {
-
-    fprintf(stderr, "%s(", sysArg->name);
-    for (int i = 0; i < sysArg->nbArg; i++) {
-
-      long long int argVal;
-      switch (i) {
-      case 0:
-        argVal = regs.rdi;
-        break;
-      case 1:
-        argVal = regs.rsi;
-        break;
-      case 2:
-        argVal = regs.rdx;
-        break;
-      case 3:
-        argVal = regs.rcx;
-        break;
-      case 4:
-        argVal = regs.r8;
-        break;
-      case 5:
-        argVal = regs.r9;
-        break;
-      default:
-        argVal = 0;
-        break;
-      }
-      if (i)
-        fprintf(stderr, ",%s = %lld", sysArg->args[i].name, argVal);
-      else
-        fprintf(stderr, "%s = %lld", sysArg->args[i].name, argVal);
+                      int syscall_number)
+{
+    if (sysArg)
+    {
+        fprintf(stderr, "%s(", sysArg->name);
+        for (int i = 0; i < sysArg->nbArg; i++)
+        {
+            long long int argVal;
+            switch (i)
+            {
+            case 0:
+                argVal = regs.rdi;
+                break;
+            case 1:
+                argVal = regs.rsi;
+                break;
+            case 2:
+                argVal = regs.rdx;
+                break;
+            case 3:
+                argVal = regs.rcx;
+                break;
+            case 4:
+                argVal = regs.r8;
+                break;
+            case 5:
+                argVal = regs.r9;
+                break;
+            default:
+                argVal = 0;
+                break;
+            }
+            if (i)
+                fprintf(stderr, ", %s = %lld", sysArg->args[i].name, argVal);
+            else
+                fprintf(stderr, "%s = %lld", sysArg->args[i].name, argVal);
+        }
+        fprintf(stderr, ") = %lld\n", regs.rax);
     }
-    fprintf(stderr, ") = %lld\n", regs.rax);
-  } else {
-    fprintf(stderr, "%s() = %lld\n", get_syscall_name(syscall_number),
-            regs.rax);
-  }
-  return;
+    else
+    {
+        fprintf(stderr, "%s() = %lld\n", get_syscall_name(syscall_number),
+                regs.rax);
+    }
+    return;
 }
 
-int main(int argc, char *argv[], char *envp[]) {
-  if (argc != 2) {
-    fprintf(stderr, "my_strace: Usage: ./my_strace <file_path>\n");
-    return 1;
-  }
+int main(int argc, char *argv[], char *envp[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "my_strace: Usage: ./my_strace <file_path>\n");
+        return 1;
+    }
 
-  int pid = fork();
-  switch (pid) {
-  case -1:
-    fprintf(stderr, "my_strace: Error: Fork failed");
-    return 2;
-  case 0:
-    ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-    execve(argv[1], argv + 1, envp);
-  default:
-    break;
-  }
+    int pid = fork();
+    switch (pid)
+    {
+    case -1:
+        fprintf(stderr, "my_strace: Error: Fork failed");
+        return 2;
+    case 0:
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execve(argv[1], argv + 1, envp);
+    default:
+        break;
+    }
 
-  int status = 0;
-  struct user_regs_struct regs = {0};
-  int syscall_in_progress = 0;
+    int status = 0;
+    struct user_regs_struct regs = { 0 };
+    int syscall_in_progress = 0;
 
-  waitpid(pid, &status, 0);
-
-  while (1) {
-    ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
     waitpid(pid, &status, 0);
 
-    if (WIFEXITED(status))
-      break;
+    while (1)
+    {
+        ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+        waitpid(pid, &status, 0);
 
-    ptrace(PTRACE_GETREGS, pid, NULL, &regs);
-    long syscall_number = regs.orig_rax;
+        if (WIFEXITED(status))
+            break;
 
-    if (syscall_in_progress) {
-      printSyscallArgs(getArgSyscall(get_syscall_name(syscall_number)), regs,
-                       syscall_number);
-      syscall_in_progress = 0;
-    } else
-      syscall_in_progress = 1;
-  }
+        ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+        long syscall_number = regs.orig_rax;
 
-  fprintf(stderr, "program exited with code %d\n", WEXITSTATUS(status));
-  return 0;
+        if (syscall_in_progress)
+        {
+            printSyscallArgs(getArgSyscall(get_syscall_name(syscall_number)),
+                             regs, syscall_number);
+            syscall_in_progress = 0;
+        }
+        else
+            syscall_in_progress = 1;
+    }
+
+    fprintf(stderr, "program exited with code %d\n", WEXITSTATUS(status));
+    return 0;
 }
