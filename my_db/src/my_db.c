52267f3b9dc1ff321fc3fab179487d2b5c8ab11e
twip4 <paulbaudinot@gmail.com>
2024-12-12 11:39:43 +0100

    fix(db): better solution for command

--- my_db/src/my_db.c
+++ my_db/src/my_db.c
@@ -7,31 +7,31 @@
 
 void print_register(struct user_regs_struct regs)
 {
-    printf("rax %lld\n", regs.rax);
-    printf("rbx %lld\n", regs.rbx);
-    printf("rcx %lld\n", regs.rcx);
-    printf("rdx %lld\n", regs.rdx);
-    printf("rsi %lld\n", regs.rsi);
-    printf("rdi %lld\n", regs.rdi);
-    printf("rbp %lld\n", regs.rbp);
-    printf("rsp %lld\n", regs.rsp);
-    printf("r8 %lld\n", regs.r8);
-    printf("r9 %lld\n", regs.r9);
-    printf("r10 %lld\n", regs.r10);
-    printf("r11 %lld\n", regs.r11);
-    printf("r12 %lld\n", regs.r12);
-    printf("r13 %lld\n", regs.r13);
-    printf("r14 %lld\n", regs.r14);
-    printf("r15 %lld\n", regs.r15);
-    printf("rip %lld\n", regs.rip);
-    printf("eflags %lld\n", regs.eflags);
-    printf("cs %lld\n", regs.cs);
-    printf("ss %lld\n", regs.ss);
-    printf("ds %lld\n", regs.ds);
-    printf("es %lld\n", regs.es);
-    printf("fs %lld\n", regs.fs);
-    printf("gs %lld\n", regs.gs);
-    printf("orig_rax %lld\n", regs.orig_rax);
+    printf("rax 0x%llx\n", regs.rax);
+    printf("rbx 0x%llx\n", regs.rbx);
+    printf("rcx 0x%llx\n", regs.rcx);
+    printf("rdx 0x%llx\n", regs.rdx);
+    printf("rsi 0x%llx\n", regs.rsi);
+    printf("rdi 0x%llx\n", regs.rdi);
+    printf("rbp 0x%llx\n", regs.rbp);
+    printf("rsp 0x%llx\n", regs.rsp);
+    printf("r8 0x%llx\n", regs.r8);
+    printf("r9 0x%llx\n", regs.r9);
+    printf("r10 0x%llx\n", regs.r10);
+    printf("r11 0x%llx\n", regs.r11);
+    printf("r12 0x%llx\n", regs.r12);
+    printf("r13 0x%llx\n", regs.r13);
+    printf("r14 0x%llx\n", regs.r14);
+    printf("r15 0x%llx\n", regs.r15);
+    printf("rip 0x%llx\n", regs.rip);
+    printf("eflags 0x%llx\n", regs.eflags);
+    printf("cs 0x%llx\n", regs.cs);
+    printf("ss 0x%llx\n", regs.ss);
+    printf("ds 0x%llx\n", regs.ds);
+    printf("es 0x%llx\n", regs.es);
+    printf("fs 0x%llx\n", regs.fs);
+    printf("gs 0x%llx\n", regs.gs);
+    printf("orig_rax 0x%llx\n", regs.orig_rax);
 }
 
 int main(int argc, char *argv[], char *envp[])
@@ -66,31 +66,31 @@ int main(int argc, char *argv[], char *envp[])
 
     while (1)
     {
-    start:
-        scanf("%s", user_input);
+        while (1)
+        {
+            scanf("%s", user_input);
 
-        if (!strcmp(user_input, "continue"))
-        {
-            ;
-        }
-        else if (!strcmp(user_input, "quit"))
-        {
-            ptrace(PTRACE_KILL, pid, NULL, NULL);
-            return 0;
-        }
-        else if (!strcmp(user_input, "kill"))
-        {
-            ptrace(PTRACE_KILL, pid, NULL, NULL);
-            break;
-        }
-        else if (!strcmp(user_input, "register"))
-        {
-            print_register(regs);
-        }
-        else
-        {
-            printf("Bad argument !!!!\n");
-            goto start;
+            if (!strcmp(user_input, "continue"))
+            {
+                break;
+            }
+            else if (!strcmp(user_input, "quit"))
+            {
+                ptrace(PTRACE_KILL, pid, NULL, NULL);
+                return 0;
+            }
+            else if (!strcmp(user_input, "kill"))
+            {
+                ptrace(PTRACE_KILL, pid, NULL, NULL);
+            }
+            else if (!strcmp(user_input, "register"))
+            {
+                print_register(regs);
+            }
+            else
+            {
+                printf("Bad argument !\n");
+            }
         }
 
         while (1)
